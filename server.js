fa676680d51e89c7cfd0a19d4a7de9b57a5c4564
Julio Balbuena <balbuenajc@hotmail.com>
2019-05-13 21:48:34 -0500

    first upload of the code

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require ("express");
+const mongoose = require ("mongoose");
+var app = express();
+//const routes = require ("./routes")
+
+const PORT = process.env.PORT || 8080;//HEROKU, LOCAL HOST
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  }
+  
+  //CIRCULAR DEPENDENCY TO USE ROUTES
+  //app.use(routes);
+  
+  //INITIALIZE APP
+  app.listen(PORT, function() {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  });
