2dba3ed062272e40498814340e4644ffddca7c90
Julio Balbuena <balbuenajc@hotmail.com>
2019-05-19 16:55:14 -0500

    node modules

--- server.js
+++ server.js
@@ -1,48 +1,47 @@
+//DEPENDECIES
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const passport = require("passport");
+const morgan = require("morgan");
 const app = express();
-const Port = process.env.PORT || 5000;
+const users = require("./routes/api/users");
+
+//PORT
+const PORT= process.env.PORT || 8080;
 
 //MIDLEWARE MORGAN
 app.use(morgan('dev'));
 //app.use(cookieParser());
 
-//routing
-//const users = require("./routes/api/users");
+//MIDLEWARE - USING EXPRESS INSTEAD OF BODYPARSER
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
-// Bodyparser middleware
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
-app.use(bodyParser.json());
+//MONGOOSE CONECTION
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/athenadb',{ useNewUrlParser: true })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
 
+
+//USE STATIC FOLDER WHILE IN HEROKU
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+//CIRCULAR DEPENDENCIES
 app.use("/api/users", users);
+
+//INITIZLIZE PASSPORT
+app.use(passport.initialize());
+require("./config/passport")(passport);
+
+
+//INITIALIZE APP
+app.listen(PORT, () => console.log(`Server up and running on port ${PORT} !`));
+
+
 
 
 // DB Config
 //const db = require("./config/keys").mongoURI;
 
-// Connect to MongoDB
-mongoose.connect(
-    process.env.MONGO_URI || 'mongodb://localhost/athenadb',
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log("MongoDB successfully connected"))
-  .catch(err => console.log(err));
-
-// Passport middleware
-app.use(passport.initialize());
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-}
-
-// Passport config
-require("./config/passport")(passport);
-
-
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
