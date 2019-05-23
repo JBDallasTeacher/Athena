cb51a9962c3a0f20ff1a7a4df8b3c5f6641f214c
Julio Balbuena <balbuenajc@hotmail.com>
2019-05-23 13:09:56 -0500

    created new routes for teacher dashboard

---
+++ client/src/components/teacher/StudentDelete.js
@@ -0,0 +1,11 @@
+import React from "react";
+//import { connect } from "react-redux";
+//import (deleteStudent) from '../../actions/studentsActions';
+
+class StudentDelete extends React.Component {
+    render() {
+        return <div>Student Delete Page</div>;
+    }
+}
+
+export default StudentDelete;
